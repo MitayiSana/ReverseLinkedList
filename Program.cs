b478b12d277d48ffba986e6580f285020e66a7e5
MitayiSana <mitayisana@gmail.com>
2024-10-17 13:33:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace ReverseLinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(10);
+            list.AddLast(20);
+            list.AddLast(30);
+            var delete = list.Reverse();
+            foreach(int i in delete)
+            {
+                Console.WriteLine(i);
+            }
+        }
+            
+    }
+}
